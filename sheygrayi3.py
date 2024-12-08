a50ef59693f9b2900e67c20c2278bfe46417e96b
mohamadhosein <mohamadhoseinsarparastfaghiran@gmail.com>
2024-12-08 01:36:52 -0800

    update and push

---
+++ sheygrayi3.py
@@ -0,0 +1,78 @@
+class volunteer:
+    def __init__(self, first_name,last_name, age, height, weight,marital,phisycal_defect):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.marital = marital
+        self.phisycal_defect = phisycal_defect
+    def save(self):
+        print(self.first_name,self.last_name,"saved")
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer1=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer1.save()
+print(volunteer1.__dict__)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer2=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer2.save()
+print(volunteer2.__dict__)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer3=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer3.save()
+print(volunteer3.__dict__)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer4=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer4.save()
+print(volunteer4.__dict__)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer5=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer5.save()
+print(volunteer5.__dict__)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+age=input("Enter your age: ")
+height=input("Enter your height: ")
+weight=input("Enter your weight: ")
+marital=input("Enter your marital: ")
+phisycal_defect=input("Enter your phisycal defect: ")
+volunteer6=volunteer(first_name,last_name, age, height, weight,marital,phisycal_defect)
+volunteer6.save()
+print(volunteer6.__dict__)
+print("سربازان مهدی")
