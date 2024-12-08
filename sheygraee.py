6e9ba5b8db42344898f7343e84088e89268b5614
mohamadhosein <mohamadhoseinsarparastfaghiran@gmail.com>
2024-12-08 00:10:30 -0800

    update

---
+++ sheygraee.py
@@ -0,0 +1,72 @@
+class Student:
+    def __init__(self, first_name, last_name,Year,code_melli,code_daneshjoo):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.Year = Year
+        self.code_melli = code_melli
+        self.code_daneshjoo = code_daneshjoo
+    def save(self):
+        print(self.first_name,self.last_name,"save")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student1=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student1.save()
+print(Student1.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student2=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student2.save()
+print(Student2.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student3=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student3.save()
+print(Student3.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student4=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student4.save()
+print(Student4.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student5=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student5.save()
+print(Student5.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student6=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student6.save()
+print(Student6.__dict__)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Year = input("Enter your year: ")
+code_melli = input("Enter your code melli: ")
+code_daneshjoo = input("Enter your code daneshjoo: ")
+Student7=Student(first_name,last_name,Year,code_melli,code_daneshjoo)
+Student7.save()
+print(Student7.__dict__)
